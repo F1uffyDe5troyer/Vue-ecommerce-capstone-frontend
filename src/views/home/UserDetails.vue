31b000c37a068a42837f1edd20b2c4654455633f
F1uffyDe5troyer <Dalarnoricardomilago@gmail.com>
2022-03-25 16:25:25 +0200

    fghjk

--- src/views/home/UserDetails.vue
+++ src/views/home/UserDetails.vue
@@ -1,7 +1,78 @@
 <template>
   <div>
-      <h1>User Profile</h1>
+<div id="container">
+  
+  <div class="sidebar">
+    
+    <div class="image">
+      
+      <img src="https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-1/c0.0.160.160/p160x160/13925229_10205028027660516_4595319040701208597_n.jpg?oh=b61eb45bb5fa0a7572f3e9800a459e64&oe=58476EC7"/>
+      
+    </div>
+    
+    <div class="bio">
+      
+      <h2>Taylor Brooke</h2>
+      
+      <p>Enamel pin woke hashtag, health goth slow-carb lumbersexual ennui lomo. Portland before they sold out kinfolk gentrify, mumblecore enamel pin cornhole live-edge godard thundercats VHS.</p>
+      
+    </div>
+    
+    <div class="links">
+      
+      <ul>
+        
+        <li>
+
+          <a href="#">Facebook</a>
+        
+        </li>
+      
+        <li>
+
+          <a href="#">Instagram</a>
+          
+        </li>
+        
+        <li>
+
+          <a href="#">Twitter</a>
+          
+        </li>
+        
+        <li>
+
+          <a href="#">Tumblr</a>
+          
+        </li>
+
+      </ul>
+
+    </div>
+    
   </div>
+  
+  <div class="post-container">
+    
+    <div class="post">
+      
+      <img src="https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-0/c0.45.206.206/p206x206/13631451_1626842660967489_4051031015401882285_n.jpg?oh=2c3d708fc3341c1b83eb26f63608a2eb&oe=58701F7D">
+      
+      <p>Ugh celiac iceland yr locavore. Craft beer offal lumbersexual, air plant woke truffaut hammock deep v umami cardigan tote bag listicle. Bespoke synth 90's artisan. Humblebrag af health goth, offal iceland thundercats helvetica bushwick etsy before they sold out flexitarian tacos echo park jianbing. Intelligentsia raw denim bitters letterpress cliche.</p>
+      
+    </div>
+    
+    <div class="post">
+      
+      <img src="https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-0/c0.45.206.206/p206x206/13631451_1626842660967489_4051031015401882285_n.jpg?oh=2c3d708fc3341c1b83eb26f63608a2eb&oe=58701F7D">
+      
+      <p>Ugh celiac iceland yr locavore. Craft beer offal lumbersexual, air plant woke truffaut hammock deep v umami cardigan tote bag listicle. Bespoke synth 90's artisan. Humblebrag af health goth, offal iceland thundercats helvetica bushwick etsy before they sold out flexitarian tacos echo park jianbing. Intelligentsia raw denim bitters letterpress cliche.</p>
+      
+    </div>
+
+  </div>
+  
+</div>  </div>
 </template>
 
 <script>
@@ -10,6 +81,6 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
